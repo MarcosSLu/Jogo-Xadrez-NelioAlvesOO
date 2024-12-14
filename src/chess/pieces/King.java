887fe7ac7a29c5e9fcfa2a233597716a21c4243a
Marcos Lú <marcoslu.dev.web@gmail.com>
2024-12-14 13:18:56 -0300

    Possiveis movimentos das peças

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -11,8 +11,14 @@ public class King extends ChessPiece{
 	}
 	
 	@Override
-	public String toString() {
+	public String toString(){
 		return "K";
+	}
+	
+	@Override
+	public boolean[][] possibleMoves(){
+		boolean[][] mat = new boolean [getBoard().getRows()][getBoard().getColumns()];
+		return mat;
 	}
 
 }
